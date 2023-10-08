3a0ab6e09133e0c99fff9f60e5ee7a9d5dbd9b52
G KEERTHESHWARAN <keertheshwaran@gmail.com>
2023-10-08 18:42:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Slider from './components/Slider';
+
+
+import LoginSignUp from './components/LoginSignUp';
+
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+
+      <Navbar/>
+      <Routes>
+          <Route path="/LoginSignUp" element={<LoginSignUp/>}>
+          </Route>
+         
+          <Route path="/" element={<Slider/>}>
+          </Route>
+          <Route path="/#trendingcourse" element={<Slider/>}>
+          </Route>
+          <Route path="/Testimonials" element={<Slider/>}>
+          </Route>
+         
+        </Routes>
+    
+      <Footer/>
+        </Router>
+
+    </>
   );
 }
+
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link}from 'react-router-dom';
+export default function Navbar() {
+  return (
+    <>
+  
+    <header className="bg-white sticky-top shadow d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-1 mb-4 px-2 border-bottom ">
+
+      <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <img src='../images/logo desktop.png' alt='' style={{width:'75px'}}/>
+        <span style={{margin:'5px',fontWeight:'bold',fontFamily: 'Autour One, cursive'}}>HuntExams</span>
+      </a>
+
+      <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><Link to="/" className="nav-link px-2 link-secondary">Home</Link></li>
+        <li><a href="/#trendingcourse" className="nav-link px-2 link-dark">Features</a></li>
+        <li><a href="/#studentfeedbacks" className="nav-link px-2 link-dark">Feedbacks</a></li>
+        <li><a href="#" className="nav-link px-2 link-dark">FAQs</a></li>
+        <li><a href="#" className="nav-link px-2 link-dark">About</a></li>
+      </ul>
+
+      <div className="col-md-3 text-end">
+        <button type="button" className="btn btn-outline-primary me-2"> <Link exact to='/LoginSignUp'>Login</Link></button>
+        <button type="button" className="btn btn-primary">Sign-up</button>
+      </div>
+    </header>
+
+    </>
+  )
+}
